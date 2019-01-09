9141ba7e0dd2eb849d52be2b9dca0d4752cf49c1
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-09 14:50:44 -0600

    fixed non-plural constraint names

--- data/02-apps.schema.sql
+++ data/02-apps.schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE apps (
   `id` int(10) NOT NULL auto_increment,
   `name` VARCHAR(128) NOT NULL,
   `type` VARCHAR(64) NOT NULL,
-  CONSTRAINT `app_key` PRIMARY KEY ( `id` ),
-  CONSTRAINT `app_ref_entities` FOREIGN KEY ( `id` ) REFERENCES `entities` ( `id` )
+  CONSTRAINT `apps_key` PRIMARY KEY ( `id` ),
+  CONSTRAINT `apps_ref_entities` FOREIGN KEY ( `id` ) REFERENCES `entities` ( `id` )
 );
