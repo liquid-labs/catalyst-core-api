71466a950c3c9fdd2a7529bca628a2372f83bdb6
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-16 14:58:23 -0600

    first draft control script for cloud_sql_proxy

--- bin/cloud-sql-proxy.sh
+++ bin/cloud-sql-proxy.sh
@@ -12,15 +12,46 @@ reset=`tput sgr0`
 
 ACTION="${1-}"
 
+# 'cloud_sql_proxy' is essentially a wrapper that spawns a second process; but killing the parent does not kill the child, so we have to fallback to process grepping
+isRunning() {
+  set +e # grep exits with error if no match
+  local PROC_COUNT=$(ps aux | grep cloud_sql_proxy | grep -v grep | wc -l)
+  set -e
+  # if [[ -f "$PID_FILE" ]] && kill -0 $(cat $PID_FILE) > /dev/null 2> /dev/null; then
+  if (( $PROC_COUNT == 0 )); then
+    return 1
+  else
+    return 0
+  fi
+}
+
 case "$ACTION" in
   name)
     echo "cloud-sql-proxy";;
   status)
-    echo "TODO";;
+    if isRunning; then
+      echo "${green}running${reset}"
+    else
+      echo "${yellow}not running${reset}"
+    fi;;
   start)
-    echo "TODO";;
+    if ! isRunning; then
+      # We were using the following to capture the pid, but see note on 'isRunning'
+      # bash -c "cd '${BASE_DIR}'; ( npx --no-install cloud_sql_proxy -instances='${CAT_SCRIPT_CORE_API_CLOUDSQL_CONNECTION_NAME}'=tcp:3306 -credential_file='${CAT_SCRIPT_CORE_API_CLOUDSQL_CREDS}' & echo \$! >&3 ) 3> '${PID_FILE}' 2> '${SERV_LOG}' &"
+      # Annoyingly, cloud_sql_proxy (at time of note) emits all logs to stderr.
+      bash -c "cd '${BASE_DIR}'; npx --no-install cloud_sql_proxy -instances='${CAT_SCRIPT_CORE_API_CLOUDSQL_CONNECTION_NAME}'=tcp:3306 -credential_file='${CAT_SCRIPT_CORE_API_CLOUDSQL_CREDS}' 2> '${SERV_LOG}' &"
+    else
+      echo "${PROCESS_NAME} appears to already be running." >&2
+    fi;;
   stop)
-    echo "TODO";;
+    if isRunning; then
+      # See note in 'start'
+      # kill $(cat "${PID_FILE}") && rm "${PID_FILE}"
+      kill $(ps aux | grep cloud_sql_proxy | grep -v grep | awk '{print $2}')
+    else
+      # TODO: use echoerr
+      echo "${PROCESS_NAME} does not appear to be running." >&2
+    fi;;
   restart)
     echo "TODO";;
   *)
