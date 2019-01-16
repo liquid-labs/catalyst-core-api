a9e78e21d2bc1735c830be3a60ef2e2145906fee
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-16 12:53:20 -0600

    skeleton process control scripts

---
+++ bin/cloud-sql-proxy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# bash strict settings
+set -o errexit # exit on errors
+set -o nounset # exit on use of uninitialized variable
+set -o pipefail
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+
+ACTION="${1-}"
+
+case "$ACTION" in
+  name)
+    echo "cloud-sql-proxy";;
+  status)
+    echo "TODO";;
+  start)
+    echo "TODO";;
+  stop)
+    echo "TODO";;
+  restart)
+    echo "TODO";;
+  *)
+    # TODO: library-ize and use 'echoerrandexit'
+    echo "Unknown action '${ACTION}'." >&2
+    exit 1;;
+esac
