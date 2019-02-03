e50a9d15516292d8f59d07b0ac21cf9c3752b25f
Zane Rockenbaugh <zane@liquid-labs.com>
2019-02-02 19:09:14 -0600

    update go server to work with starting multiple services
    also support option of passthru args

--- bin/go-dev-server.sh
+++ bin/go-dev-server.sh
@@ -10,14 +10,43 @@ isRunning() {
 }
 
 start() {
-  local PORT_OPT=''
-  local PORT_KEY="$( echo ${SERV_NAME} | tr '-' '_' | tr 'a-z' 'A-Z')_PORT"
-  if [[ -n "${!PORT_KEY:-}" ]]; then
-    PORT_OPT="--port=${!PORT_KEY}"
+  local CORE_APP_PATH="${1:-}"
+  local CMD="dev_appserver.py "
+
+  if [[ -z "$CORE_APP_PATH" ]]; then
+    local MY_ROOT="${BASH_SOURCE[0]}"
+    if [[ -L "$MY_ROOT" ]]; then
+      cd $(dirname "${MY_ROOT}")
+      MY_ROOT=$(readlink "${MY_ROOT}")
+    fi
+    cd $(dirname "${MY_ROOT}")
+    MY_ROOT=$(dirname "$PWD")
+    CORE_APP_PATH="${MY_ROOT}/go/app.yaml"
   fi
 
+  local APPS="${CORE_APP_PATH}"
+  for REQ_PARAM in $REQ_PARAMS; do
+    CMD="${CMD} --env_var $REQ_PARAM='${!REQ_PARAM}'"
+    if [[ "${REQ_PARAM}" == "ADD_GO_APP_"* ]]; then
+      local APP_PACK=$(echo "${!REQ_PARAM}" | cut -d: -f1)
+      local APP_PATH=$(echo "${!REQ_PARAM}" | cut -d: -f2)
+      local APP_ABS_PATH
+      if [[ "$PACKAGE_NAME" == "$APP_PACK" ]]; then
+        APP_ABS_PATH="${BASE_DIR}/${APP_PATH}"
+      else
+        APP_ABS_PATH="${MY_ROOT}/node_modules/${APP_PACK}/${APP_PATH}"
+      fi
+      if [[ ! -f "$APP_ABS_PATH" ]]; then
+        echo "Did not find expected app file '${!REQ_PARAM}' ($APP_ABS_PATH)." >&2
+        exit 4
+      fi
+      APPS="${APPS} ${APP_ABS_PATH}"
+    fi
+  done
+
   # Boo! dev_appserver uses stderr for logs.
-  cd ${BASE_DIR}/go && ( dev_appserver.py ${PORT_OPT} --env_var FIRBASE_DB_URL="${FIREBASE_DB_URL}" app.yaml 2>&1 & echo $! > "${PID_FILE}" ) > "${SERV_LOG}"
+  CMD="${CMD} ${APPS} "$@" > '${SERV_LOG}' 2>&1 & echo \$! > '${PID_FILE}'"
+  eval "$CMD"
 }
 
 stop() {
@@ -25,7 +54,7 @@ stop() {
     || echo 'There may have been a problem shutting down the go dev server. Check manually.'"
 }
 
-ACTION="${1-}"
+ACTION="${1-}"; shift
 
 case "$ACTION" in
   name)
@@ -39,7 +68,7 @@ case "$ACTION" in
       echo "stopped"
     fi;;
   start)
-    start;;
+    start "$@";;
   stop)
     stop;;
   restart)
