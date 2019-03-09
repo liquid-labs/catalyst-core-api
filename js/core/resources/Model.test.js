00341f3d0f54c27b5890a163080593d98c467671
Zane Rockenbaugh <zane@liquid-labs.com>
2019-03-09 15:30:41 -0600

    first test directly on Model

---
+++ js/core/resources/Model.test.js
@@ -0,0 +1,18 @@
+/* global describe expect test */
+import { Model } from './Model'
+
+const fooPropsModel = [
+  {propName : 'name', unsetForNew : false, writable : true}
+]
+
+const Foo = class extends Model {
+  get resourceName() { return 'foos' }
+}
+Model.finalizeConstructor(Foo, fooPropsModel)
+
+describe('Model', () => {
+  test("'forApi()' should set empty, plain (non-model) values to null", () => {
+    const foo = new Foo({name: ""})
+    expect(foo.forApi().name).toBeNull()
+  })
+})
